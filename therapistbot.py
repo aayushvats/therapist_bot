b287a9a86998da9a6f4e806f3eb3965dd4d5402b
Bo <bohedenius@gmail.com>
2019-10-24 15:23:23 +0200

    fixed issue where Carol would reply twice after an 'if'-statement

--- therapistbot.py
+++ therapistbot.py
@@ -3,9 +3,10 @@
 
 #Default data
 inpatient = 0
+blankinput = 0
 
 suicidalsigns = list()
-suicidalsigns = ['suicidal', 'die', 'death', 'suicide']
+suicidalsigns = ['suicidal', 'die', 'death', 'suicide', 'kill']
 
 #Greeting
 print('Hello. My name is Carol and I am your new therapist, what is your name?')
@@ -17,7 +18,7 @@ print()
 therapistphrase = ('I see...', ('How did that make you feel, ' + str(name) + '?'), 'Do you want to have it this way?', 'What makes you feel like that?')
 maxval = len(therapistphrase) - 1
 
-print('Carol: I see, ' + str(name) + ', nice to meet you.')
+print('Carol: I see, ' + str(name) + '. Nice to meet you.')
 print('       So how are you today, ' + str(name) + '?')
 print()
 
@@ -33,20 +34,28 @@ while True:
     print()
 
     if message == 'no':
-        print('Carol: Well will you do anything about it?')
-        print()
-        willu = input('You: ')
-        print()
-        if willu == 'no':
-            print('Carol: ' + str(randomtherapistphrase))
+            print('Carol: Well will you do anything about it?')
             print()
-            continue
+            willu = input('You: ')
+            print()
+            
+
+    if message == '':
+        print('It will not get better if you do not talk to me, ' + str(name))
 
     for a in suicidalsigns:
         if a in message:
-            print('That really worries me, ' + str(name) + ', we will now put you inpatient')
-            inpatient += 1
-            print()
+            if inpatient == 0:
+                print('Carol: That really worries me, ' + str(name) + ', we will now put you inpatient')
+                print()
+                inpatient += 1
+                break
+
+            if inpatient > 0:
+                print('Carol: We will not let you out of here if you keep saying those things, ' + str(name))
+                print()
+                inpatient += 1
+                break
 
     else:
         print('Carol: ' + str(randomtherapistphrase))
